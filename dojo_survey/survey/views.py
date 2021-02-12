dbd5b54294855d914d0a81874b38e7fb3b91d553
Caleb A Comstock <c_comstock@rocketmail.com>
2021-02-12 15:46:23 -0800

    added additional types of form inputs

--- dojo_survey/survey/views.py
+++ dojo_survey/survey/views.py
@@ -9,13 +9,22 @@ def index(request):
 
 def result(request):
 	print(request.POST)
+	if 'favorite' in request.POST:
+		fav = request.POST['favorite']
+	else:
+		fav = "Not Specified"
+	if 'robot' in request.POST:
+		robot = 'You Are Not A Robot'
+	else:
+		robot = 'You Are A Robot'
 	context = {
 		"title": "Dojo Survey",
 		"name_from_form": request.POST['name'],
 		"email_from_form": request.POST['email'],
 		"location_from_form": request.POST['location'],
-		"favLang_from_form": request.POST['favorite'],
+		"favLang_from_form": fav,
 		"comments_from_form": request.POST['comments'],
+		"robot_from_form": robot
 	}
 	return render(request, "success.html", context)
 
